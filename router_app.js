806e56a62e3908a7b71b72cbda2197b2d6efc77c
dluccajose <dluccajose@gmail.com>
2018-12-04 22:22:15 -0400

    añadido a dashboard: numero de imagenes subidas, numero de imagenes subidas por el usuario, usuarios registrados

--- router_app.js
+++ router_app.js
@@ -4,9 +4,38 @@ var Imagen = require("./models/imagen").Imagen;
 var Usuario = require("./models/user").Usuario;
 var fs = require("fs");
 
+
 router.route("/")
-    .get(function(req,res){
-         res.render("app/home");
+
+    .get(function (req, res) {
+
+        
+        Imagen.find(function (err, doc) {
+
+            var total_imagenes = doc.length    // Numero de imagenes subidas
+            var tus_imagenes = 0;
+
+            // Contar imagenes subidas por el usuario
+            for (var i = 0; i < doc.length; i++) {
+                if (doc[i].owner.toString() === res.locals.user._id.toString()) {
+                    tus_imagenes++;
+                }
+            }
+
+            Usuario.find(function (err, user_doc) {
+
+                var total_usuarios = user_doc.length;   // Numero de usuarios registrados
+
+                res.render("app/home", {
+                    total_imagenes: total_imagenes,
+                    tus_imagenes: tus_imagenes,
+                    total_usuarios: total_usuarios
+                });
+
+            });
+
+        });
+
     })
 
 
