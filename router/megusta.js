3dcc8b3435bdfe50ec5e2f2686bf48e95f8da05b
dluccajose <dluccajose@gmail.com>
2018-12-11 18:09:02 -0400

    Terminada funcion Me Gusta

--- router/megusta.js
+++ router/megusta.js
@@ -15,35 +15,83 @@ router.route("/megusta/:id")
             imagen: req.params.id
         });
 
-        megusta.save(function (err) {
+        // Verificamos que el usuario no le haya dado me gusta antes
+        Megusta.find({usuario: res.locals.user._id, imagen: req.params.id}, function(err,doc) {
 
-            if (!err) {
-                console.log("Me Gusta guardado con exito");
+            if(doc.length === 0) {
 
-                Imagen.findById(req.params.id, function (err, doc) {    // Buscamos la imagen a actualizar
+                megusta.save(function (err) {
 
                     if (!err) {
-                        doc.megusta_count++;    // Aumentamos el conteo de Megusta
-                        doc.save(function (err) {
-
+                        console.log("Me Gusta guardado con exito");
+        
+                        Imagen.findById(req.params.id, function (err, doc) {    // Buscamos la imagen a actualizar
+        
                             if (!err) {
-                                console.log("conteo actualizado correctamente");
+                                doc.megusta_count++;    // Aumentamos el conteo de Megusta
+                                doc.save(function (err) {
+        
+                                    if (!err) {
+                                        console.log("conteo actualizado correctamente");
+                                    }
+                                });
+                            } else {
+                                res.render("app/error", { error: err });
+                                console.log(err);
                             }
                         });
+        
+                        res.redirect("/app/imagenes/" + req.params.id);
+        
                     } else {
                         res.render("app/error", { error: err });
                         console.log(err);
                     }
                 });
 
-                res.redirect("/app/imagenes/" + req.params.id);
-
             } else {
+                res.render("app/error", { error: 'Ya diste me gusta a esta publicacion' });
+            }
+        })
+
+        
+
+    })
+    
+    .delete(function(req, res) {
+
+        Megusta.findOne({usuario: res.locals.user._id, imagen: req.params.id}, function(err, megusta) {
+
+            if(!err) {
+                if(megusta) {
+                megusta.delete(function(err) {
+
+                    if(!err) {
+
+                        Imagen.findById(req.params.id, function(err, doc) {
+                            doc.megusta_count--;
+                            doc.save(function(err) {
+                                res.redirect("/app/imagenes/" + req.params.id);
+                            })
+                        })
+
+                    } else {
+
+                        res.render("app/error", { error: err });
+                        console.log(err);
+
+                    }
+                });
+                } else {
+                    res.render("app/error", { error: "Error" });
+                }
+            } else {
+
                 res.render("app/error", { error: err });
                 console.log(err);
+
             }
         });
-
     });
 
     module.exports = router;
